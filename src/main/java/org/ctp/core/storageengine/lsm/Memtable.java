0d90f2391661173f266b4fa27b5adb5dccec1785
Bo Zhou <zb.public@outlook.com>
2018-07-16 10:35:09 +0800

    Implementing the Memtable && SSTable

--- src/main/java/org/ctp/core/storageengine/lsm/Memtable.java
+++ src/main/java/org/ctp/core/storageengine/lsm/Memtable.java
@@ -1,4 +1,67 @@
 package org.ctp.core.storageengine.lsm;
 
-public class Memtable {
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class Memtable implements Iterable<Pair<String, String>>  {
+    private int version = 0;
+    private int rawSize = 0;
+    private SortedMap<String, String> map = new TreeMap<String, String>();
+
+    public Memtable() {
+
+    }
+
+    public void put(String key, String value) {
+        if (key == null || key.length() == 0)
+            throw new IllegalArgumentException();
+        if (value != null || value.length() == 0)
+            throw new IllegalArgumentException();
+
+        map.put(key, value);
+        rawSize += (key.getBytes().length + value.getBytes().length);
+        version++;
+    }
+
+    public String read(String key) {
+        return map.get(key);
+    }
+
+    public int getRawSize() {
+        return rawSize;
+    }
+
+    @Override
+    public Iterator<Pair<String, String>> iterator() {
+        return new MemtableIterator(version);
+    }
+
+    private class MemtableIterator implements Iterator<Pair<String, String>> {
+        private final int snapshotVersion;
+        private final Iterator<String> iterator;
+
+        public MemtableIterator(int version) {
+            this.snapshotVersion = version;
+            iterator = map.keySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (snapshotVersion != version) {
+                throw new IllegalStateException();
+            }
+            return iterator.hasNext();
+
+        }
+
+        @Override
+        public Pair<String, String> next() {
+            if (snapshotVersion != version) {
+                throw new IllegalStateException();
+            }
+            String key = iterator.next();
+            return new Pair<>(key, map.get(key));
+        }
+    }
 }
--- src/main/java/org/ctp/core/storageengine/lsm/SSTable.java
+++ src/main/java/org/ctp/core/storageengine/lsm/SSTable.java
@@ -1,4 +1,120 @@
 package org.ctp.core.storageengine.lsm;
 
+import javafx.util.Pair;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.FileAlreadyExistsException;
+
 public class SSTable {
+    private final byte[] HEADER = {'C', 'T', 'P', 'D', 'K', 'V'};
+
+    private final String filename;
+
+
+    public SSTable(String sstableFilename) {
+        filename = sstableFilename;
+    }
+
+    public static SSTable flush(Memtable memtable, String sstableFilename) throws IOException {
+        SSTable newSSTable = new SSTable(sstableFilename);
+        newSSTable.flush(memtable);
+        return newSSTable;
+    }
+
+    public InMemIndex buildIndex() throws IOException {
+        try (DataInputStream inputStream = new DataInputStream(new FileInputStream(filename))) {
+            verifyIntegrity(inputStream);
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private void flush(Memtable memtable) throws IOException {
+        File file = new File(filename);
+        if (file.exists() && file.isFile()) {
+            throw new FileAlreadyExistsException("The file " + filename + " was already existed");
+        }
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(filename))) {
+            for (Pair<String, String> entry : memtable) {
+                appendItem(outputStream, entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    private void appendItem(DataOutputStream outputStream, String key, String value) throws IOException {
+        if (key == null || key.length() == 0) {
+            throw new IllegalArgumentException("The key is null or empty");
+        }
+        if (value != null && value.length() == 0) {
+            throw new IllegalArgumentException("The value is empty");
+        }
+
+        byte[] keyBytes = key.getBytes(Charset.defaultCharset());
+        if (keyBytes.length > 127) {
+            throw new IllegalArgumentException("The key exceed the length");
+        }
+
+        byte[] valueBytes = value.getBytes(Charset.defaultCharset());
+        if (valueBytes.length > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("The value exceed the length");
+        }
+
+        outputStream.writeByte(keyBytes.length);
+        outputStream.write(keyBytes);
+        outputStream.write(valueBytes.length);
+        if (valueBytes.length > 0) {
+            outputStream.write(valueBytes);
+        }
+    }
+
+    private Pair<String, String> readItem(DataInputStream inputStream) throws IOException {
+        int keyLength = inputStream.readByte();
+        if (keyLength < 0) {
+            throw new IllegalArgumentException();
+        }
+        byte[] keyBuf = new byte[keyLength];
+        inputStream.read(keyBuf);
+        String key = new String(keyBuf);
+
+        int valueLength = inputStream.readInt();
+        if (valueLength < 0) {
+            throw new IllegalArgumentException();
+        }
+        String value = null;
+        if (valueLength > 0) {
+            byte[] valueBuf = new byte[valueLength];
+            inputStream.read(valueBuf);
+            value = new String(valueBuf);
+        }
+
+        return new Pair<>(key, value);
+    }
+
+    private void verifyIntegrity(DataInputStream inputStream) throws IOException {
+        checkHeader(inputStream);
+    }
+
+    private boolean checkHeader(DataInputStream inputStream) throws IOException {
+        byte[] header = new byte[HEADER.length];
+        inputStream.read(header);
+
+        for (int i = 0; i < header.length; i++) {
+            if (header[i] != HEADER[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void setHeader(OutputStream outputStream) throws IOException {
+        outputStream.write(HEADER);
+    }
+
+    private int getHeaderSize() {
+        return HEADER.length;
+    }
+
 }
