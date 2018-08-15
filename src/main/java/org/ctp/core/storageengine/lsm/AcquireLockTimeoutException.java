f85aa6d5ceaec5b79dac83d75c2f6f49e5e1f021
Bo Zhou <zb.public@outlook.com>
2018-08-15 15:40:55 +0800

    Using the background thread to flushing the full memtable

---
+++ src/main/java/org/ctp/core/storageengine/lsm/AcquireLockTimeoutException.java
@@ -0,0 +1,11 @@
+package org.ctp.core.storageengine.lsm;
+
+public class AcquireLockTimeoutException extends RuntimeException {
+        public AcquireLockTimeoutException() { super(); }
+
+        public AcquireLockTimeoutException(String message) { super(message); }
+
+        public AcquireLockTimeoutException(String message, Throwable cause) { super(message, cause); }
+
+        public AcquireLockTimeoutException(Throwable cause) { super(cause); }
+}
